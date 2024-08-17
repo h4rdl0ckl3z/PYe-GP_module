a93786f37a8293166e10e5b2fc0665f4dc402563
Avatart0Dev <54482333+h4rdl0ckl3z@users.noreply.github.com>
2024-08-17 23:49:28 +0700

    Create e_GP.py

---
+++ e_GP.py
@@ -0,0 +1,46 @@
+class e_GP:
+    def egp(self, deptId_):
+        from urllib.request import urlopen
+        from urllib.error import URLError, HTTPError
+        import xml.etree.ElementTree as ET
+
+        url = 'http://process3.gprocurement.go.th/EPROCRssFeedWeb/egpannouncerss.xml'
+        parameter_deptId = '?deptId='
+        parameter_anounceType = '&anounceType='
+
+        anounceType_ = ['W0', 'W2', 'B0', 'D0', 'D1', 'D2', 'P0', 'W1', '15']
+
+        list_data = {
+            'title': [],
+            'link': [],
+            'pubDate': [],
+            'pubT': []
+        }
+
+        for anounceType in anounceType_:
+            for deptId in deptId_:
+                url_str = url + parameter_deptId + deptId + parameter_anounceType + anounceType
+                try:
+                    res = urlopen(url_str)
+                    try:
+                        tree = ET.parse(res).getroot()
+                        for root in tree.findall('./channel/item'):
+                            if root.tag == 'item':
+                                list_data['pubT'].append(anounceType)
+                                for rss in root:
+                                    if rss.tag == 'description' or rss.tag == 'guid':
+                                        pass
+                                    else:
+                                        list_data[rss.tag].append(rss.text)
+                            else:
+                                print('No ITEMS')
+
+                    except ET.ParseError as err:
+                        print(err)
+                except (URLError, HTTPError, ConnectionError) as err:
+                    print(err)
+        return list_data
+
+# example
+test = e_GP()
+print(test.egp(['6840502']))
