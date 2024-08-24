5fdfe9661e357757370da5ca27205a5ef86958b8
Avatart0Dev <54482333+h4rdl0ckl3z@users.noreply.github.com>
2024-08-24 22:00:46 +0700

    Update e_GP.py

--- e_GP.py
+++ e_GP.py
@@ -7,13 +7,7 @@ class e_GP:
         base_url = 'http://process3.gprocurement.go.th/EPROCRssFeedWeb/egpannouncerss.xml'
         anounce_types = ['W0', 'W2', 'B0', 'D0', 'D1', 'D2', 'P0', 'W1', '15']
 
-        list_data = {
-            'title': [],
-            'link': [],
-            'pubDate': [],
-            'anounceType': [],
-            'egpid': []
-        }
+        myData = []
 
         try:
             for anounce_type in anounce_types:
@@ -21,13 +15,15 @@ class e_GP:
                     url = f"{base_url}?deptId={dept_id}&anounceType={anounce_type}"
                     res = urlopen(url)
                     tree = ET.parse(source=res, parser=ET.XMLParser(encoding='cp874')).getroot()
+                    list_data = {}
                     for root in tree.findall('./channel/item'):
-                        list_data['egpid'].append(dept_id)
-                        list_data['anounceType'].append(anounce_type)
                         for rss in root:
                             if rss.tag not in ('description', 'guid'):
-                                list_data[rss.tag].append(rss.text)
+                                list_data[rss.tag] = rss.text
+                        list_data['egpid'] = dept_id
+                        list_data['anounceType'] = anounce_type
+                        myData.append(list_data)
         except (URLError, HTTPError, ConnectionError, ET.ParseError) as err:
             print(f"Error fetching data: {err}")
 
-        return list_data
+        return myData
