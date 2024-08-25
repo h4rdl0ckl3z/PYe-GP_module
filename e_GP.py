1b2171bcd691972a8a030416ff2dc7cccd341366
Avatart0Dev <54482333+h4rdl0ckl3z@users.noreply.github.com>
2024-08-25 17:57:05 +0700

    Update e_GP.py

--- e_GP.py
+++ e_GP.py
@@ -1,23 +1,21 @@
 class e_GP:
-    def __new__(self, dept_ids):
-        self.base_url = 'http://process3.gprocurement.go.th/EPROCRssFeedWeb/egpannouncerss.xml'
+    def __init__(self, dept_ids):
         anounce_types = ['W0', 'W2', 'B0', 'D0', 'D1', 'D2', 'P0', 'W1', '15']
-        
+
         self.myData = []
 
         for anounce_type in anounce_types:
             for dept_id in (i for i in dept_ids if i not in ('')):
                 self.fetchData(dept_id, anounce_type)
-        
-        return self.myData
 
     def fetchData(self, dept_id, anounce_type):
         from urllib.request import urlopen
         from urllib.error import URLError, HTTPError
         import xml.etree.ElementTree as ET
 
+        base_url = 'http://process3.gprocurement.go.th/EPROCRssFeedWeb/egpannouncerss.xml'
         try:
-            url = f'{self.base_url}?deptId={dept_id}&anounceType={anounce_type}'
+            url = f'{base_url}?deptId={dept_id}&anounceType={anounce_type}'
             res = urlopen(url)
             tree = ET.parse(source=res, parser=ET.XMLParser(encoding='cp874')).getroot()
             list_data = {}
@@ -25,7 +23,10 @@ class e_GP:
                 list_data = {rss.tag: rss.text for rss in item if rss.tag not in ('description', 'guid')}
                 list_data['anounceType'] = anounce_type
                 list_data['egpid'] = dept_id
-            self.myData.append(list_data)
-            list_data.clear()
+                self.myData.append(list_data)
         except (URLError, HTTPError, ConnectionError, ET.ParseError) as err:
             print(f'Error fetching data for dept_id {dept_id} and anounce_type {anounce_type}: {err}')
+
+    def to_json(self):
+        from json import dumps
+        return dumps(self.myData, ensure_ascii=False, indent=4)
